1a32685353f10baaa809dfa17b7bfb14e4b9bc72
unknown <bogosavljevicivan72@gmail.com>
2023-11-13 22:15:12 +0100

    Rest API now connected to database, Database fixed added IDs

--- Proj1/Rest/data.py
+++ Proj1/Rest/data.py
@@ -1,8 +1,53 @@
+import sqlite3
 
-IoTReadings = [
-    {"id": 1, "temperature" : 24.0, "ph": 3.0,"turbidity" : 5.0, "BOD": 15.0,"fecal" : 20.0, "oxygen": 50.0,"nitr": 100.0, "conductivity" : 150.0},
-    {"id": 2, "temperature" : 25.0, "ph": 4.0,"turbidity" : 6.0, "BOD": 16.0,"fecal" : 21.0, "oxygen": 51.0,"nitr": 101.0, "conductivity" : 151.0},
-    {"id": 3, "temperature" : 26.0, "ph": 5.0,"turbidity" : 7.0, "BOD": 17.0,"fecal" : 22.0, "oxygen": 52.0,"nitr": 102.0, "conductivity" : 152.0},
-    {"id": 4, "temperature" : 26.0, "ph": 5.0,"turbidity" : 7.0, "BOD": 17.0,"fecal" : 22.0, "oxygen": 52.0,"nitr": 102.0, "conductivity" : 152.0}
-]
+databaseName = "IOTMeterData.db"
+conn = sqlite3.connect('..\Database\IOTMeterData.db', check_same_thread=False)
 
+c = conn.cursor()
+
+def GetAll():
+    c.execute("SELECT * FROM IOTMeterData")
+    returnValue = c.fetchall()
+    conn.commit()
+    return returnValue
+
+def Get(id):
+    c.execute("SELECT * FROM IOTMeterData WHERE ID=:id", {"id": id})
+    returnValue = c.fetchone()
+    conn.commit()
+    return list(returnValue)
+
+def Create(reading):
+    c.execute("""INSERT INTO IOTMeterData ("Temperature(C)", "pH", "Turbidity(NTU)", "BOD(mg/l)", "FecalColiform(MPN/100ml)", "DisolvedOxygen(mg/l)", "NITRATENANN+NITRITENANN(mg/l)", "Conductivity(micro_mhos/cm)")
+              VALUES (:temperature, :ph, :turbidity, :BOD, :fecal, :oxygen, :nitr, :conductivity)""", reading)
+    conn.commit()
+    return reading
+
+
+def Update(reading):
+    c.execute('''
+        UPDATE IOTMeterData
+        SET "Temperature(C)" = :temperature,
+            "pH" = :ph,
+            "Turbidity(NTU)" = :turbidity,
+            "BOD(mg/l)" = :BOD,
+            "FecalColiform(MPN/100ml)" = :fecal,
+            "DisolvedOxygen(mg/l)" = :oxygen,
+            "NITRATENANN+NITRITENANN(mg/l)" = :nitr,
+            "Conductivity(micro_mhos/cm)" = :conductivity
+        WHERE ID = :id
+    ''', reading)
+    conn.commit()
+    return reading
+
+def Delete(id):
+    c.execute("DELETE FROM IOTMeterData WHERE ID=:id", {"id": id})
+    conn.commit()
+    return "Succssus"
+
+# IoTReadings = [
+#     {"id": 1, "temperature" : 24.0, "ph": 3.0,"turbidity" : 5.0, "BOD": 15.0,"fecal" : 20.0, "oxygen": 50.0,"nitr": 100.0, "conductivity" : 150.0},
+#     {"id": 2, "temperature" : 25.0, "ph": 4.0,"turbidity" : 6.0, "BOD": 16.0,"fecal" : 21.0, "oxygen": 51.0,"nitr": 101.0, "conductivity" : 151.0},
+#     {"id": 3, "temperature" : 26.0, "ph": 5.0,"turbidity" : 7.0, "BOD": 17.0,"fecal" : 22.0, "oxygen": 52.0,"nitr": 102.0, "conductivity" : 152.0},
+#     {"id": 4, "temperature" : 26.0, "ph": 5.0,"turbidity" : 7.0, "BOD": 17.0,"fecal" : 22.0, "oxygen": 52.0,"nitr": 102.0, "conductivity" : 152.0}
+# ]
--- Proj1/Rest/main.py
+++ Proj1/Rest/main.py
@@ -1,29 +1,25 @@
 from flask import Flask, jsonify, request
-from data import IoTReadings
+from data import GetAll,Get,Create,Update,Delete
 app = Flask(__name__)
 
 
 #Route get all Readings
 @app.route('/iots', methods=['GET'])
 def get_iots():
-    return IoTReadings
+    return GetAll()
 
 #Route Get spcific Reading
 @app.route('/iots/<int:iot_id>', methods=['GET'])
 def get_reading(iot_id):
-    for reading in IoTReadings:
-        if reading['id']==iot_id:
-            return reading
+    return Get(iot_id)
         
-    return {'error':'IoT not found'}
 
 #Route Crate Readings
 #use this for body V
 #{ "temperature" : 29.0, "ph": 9.0,"turbidity" : 9.0, "BOD": 18.0,"fecal" : 28.0, "oxygen": 58.0,"nitr": 108.0, "conductivity" : 158.0}
 @app.route('/iots', methods=['POST'])
 def create_reading():
-    new_reading={'id':len(IoTReadings)+1,
-                 'temperature':request.json['temperature'], 
+    new_reading={'temperature':request.json['temperature'], 
                  'ph':request.json['ph'],
                  'turbidity':request.json['turbidity'],
                  'BOD':request.json['BOD'],
@@ -31,35 +27,28 @@ def create_reading():
                  'oxygen':request.json['oxygen'],
                  'nitr':request.json['nitr'],
                  'conductivity':request.json['conductivity']}
-    IoTReadings.append(new_reading)
-    return new_reading
+    return Create(new_reading)
 
 #Route Update a Readings
 @app.route('/iots/<int:iot_id>', methods=['PUT'])
 def update_book(iot_id):
-    for reading in IoTReadings:
-        if reading['id']==iot_id:
-            reading['temperature']=request.json['temperature']
-            reading['ph']=request.json['ph']
-            reading['turbidity']=request.json['turbidity']
-            reading['BOD']=request.json['BOD']
-            reading['fecal']=request.json['fecal']
-            reading['oxygen']=request.json['oxygen']
-            reading['nitr']=request.json['nitr']
-            reading['conductivity']=request.json['conductivity']
-            return reading 
+    reading = Get(iot_id)
+    if reading[0]==iot_id:
+        reading[0]=request.json['temperature']
+        reading[1]=request.json['ph']
+        reading[2]=request.json['turbidity']
+        reading[3]=request.json['BOD']
+        reading[4]=request.json['fecal']
+        reading[5]=request.json['oxygen']
+        reading[6]=request.json['nitr']
+        reading[7]=request.json['conductivity']
+        reading[8]=iot_id
+    return Update(reading) 
         
-    return {'error':'Reading not found'}
-
 #Route Delete a Readings
 @app.route('/iots/<int:iot_id>', methods=['DELETE'])
 def delete_book(iot_id):
-    for reading in IoTReadings:
-        if reading['id']==iot_id:
-            IoTReadings.remove(reading)
-            return {"data":"Reading Deleted Successfully"}
-
-    return {'error':'Reading not found'}
+    return Delete(iot_id)
 
 
 #Runing
