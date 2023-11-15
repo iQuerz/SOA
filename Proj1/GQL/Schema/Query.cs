ef74af2d2147392c159a3ee1b1cf8bb1429ef22d
iQuerz <niki.tenis@gmail.com>
2023-11-15 18:31:54 +0100

    Add GraphQL project

--- Proj1/GQL/Schema/Query.cs
+++ Proj1/GQL/Schema/Query.cs
@@ -1,19 +1,51 @@
-﻿using Bogus;
+﻿using Microsoft.Data.Sqlite;
 
 namespace GQL.Schema
 {
     public class Query
     {
-        public IEnumerable<WaterReadingType> GetReadings()
-        {
-            Faker<WaterReadingType> faker = new();
-            return faker.Generate(5);
-        }
+        readonly string connectionString = "Data Source=../Database/iot_telemetry_data.db;";
 
-        public IEnumerable<WaterReadingType> GetReadingsByFilter()
+        public async Task<List<SensorReadingType>> GetReadings(int take = 10, string device = "", bool sort = true)
         {
-            Faker<WaterReadingType> faker = new();
-            return faker.Generate(5);
+            var result = new List<SensorReadingType>();
+
+            var where = string.IsNullOrEmpty(device) ? string.Empty : $"WHERE device='{device}'";
+            var limit = $"LIMIT {take}";
+            var order = sort ? $"ORDER BY ts ASC" : "";
+
+            string query = $"SELECT * FROM iot_telemetry_data" +
+                           $"\n{where}" +
+                           $"\n{order}" +
+                           $"\n{limit}";
+
+            SQLitePCL.Batteries.Init();
+
+            using (SqliteConnection connection = new(connectionString))
+            {
+                await connection.OpenAsync();
+                using SqliteCommand command = new(query, connection);
+                using SqliteDataReader reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    var i = 0;
+                    result.Add(new SensorReadingType
+                    {
+                        ts = reader.GetString(i++),
+                        device = reader.GetString(i++),
+                        co = reader.GetDouble(i++),
+                        humidity = reader.GetDouble(i++),
+                        light = reader.GetBoolean(i++),
+                        lpg = reader.GetDouble(i++),
+                        motion = reader.GetBoolean(i++),
+                        smoke = reader.GetDouble(i++),
+                        temp = reader.GetDouble(i++),
+                    });
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ Proj1/GQL/Schema/SensorReading.cs
@@ -0,0 +1,16 @@
+﻿namespace GQL.Schema
+{
+    public class SensorReadingType
+    {
+        public string ts {  get; set; }
+        public string date => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(Convert.ToDouble(ts)).ToLocalTime().ToString("HH:mm dd.MM.yyyy");
+        public string device { get; set; }
+        public double co { get; set; }
+        public double humidity { get; set; }
+        public bool light { get; set; }
+        public double lpg { get; set; }
+        public bool motion { get; set; }
+        public double smoke { get; set; }
+        public double temp { get; set; }
+    }
+}
